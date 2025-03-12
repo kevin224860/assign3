05b58465bfe484d0ece865563404e3a743627c7a
kevin <kevinsheehan006@gmail.com>
2025-03-11 21:12:29 -0400

    first commit

---
+++ tests/test_app.py
@@ -0,0 +1,24 @@
+import pytest
+
+from myapp.app import multiply_by_two, divide_by_two
+
+
+@pytest.fixture
+def numbers():
+    a = 10
+    b = 20
+    return [a,b]
+
+
+class TestApp:
+    def test_multiplication(self, numbers):
+        res = multiply_by_two(numbers[0])
+        assert res == numbers[1]
+
+    def test_division(self, numbers):
+        res = divide_by_two(numbers[1])
+        assert res == numbers[0]
+
+    def test_assignment_q_one(self):
+        res = multiply_by_two(18) 
+        assert res != 36
